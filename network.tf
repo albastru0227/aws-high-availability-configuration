07451c77bc0dabf59be963c6c730240786ec4861
albas <albastru0227gmail.com>
2026-05-26 20:18:12 +0900

    サブネットを作成

--- network.tf
+++ network.tf
@@ -6,3 +6,39 @@ resource "aws_vpc" "my_vpc" {
     Name = "my_vpc"
   }
 }
+
+resource "aws_subnet" "my_subnet_public_1a" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = var.public_subnet_1a_cidr
+  availability_zone = "ap-northeast-1a"
+  tags = {
+    Name = "my_subnet_public_a"
+  }
+}
+
+resource "aws_subnet" "my_subnet_public_1c" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = var.public_subnet_1c_cidr
+  availability_zone = "ap-northeast-1c"
+  tags = {
+    Name = "my_subnet_public_c"
+  }
+}
+
+resource "aws_subnet" "my_subnet_private_1a" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = var.private_subnet_1a_cidr
+  availability_zone = "ap-northeast-1a"
+  tags = {
+    Name = "my_subnet_private_1a"
+  }
+}
+
+resource "aws_subnet" "my_subnet_private_1c" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = var.private_subnet_1c_cidr
+  availability_zone = "ap-northeast-1c"
+  tags = {
+    Name = "my_subnet_private_1c"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -9,3 +9,27 @@ variable "vpc_cidr" {
   type = string
   default = "10.0.0.0/16"
 }
+
+variable "public_subnet_1a_cidr" {
+  description = "AZがap-northeast-1aのパブリックサブネットのCIDR"
+  type = string
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_1c_cidr" {
+  description = "AZがap-northeast-1cのパブリックサブネットのCIDR"
+  type = string
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_1a_cidr" {
+  description = "AZがap-northeast-1aのプライベートサブネットのCIDR"
+  type = string
+  default = "10.0.10.0/24"
+}
+
+variable "private_subnet_1c_cidr" {
+  description = "AZがap-northeast-1cのプライベートサブネットのCIDR"
+  type = string
+  default = "10.0.20.0/24"
+}
