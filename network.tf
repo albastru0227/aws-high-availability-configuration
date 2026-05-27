311175921aefcc021572b2402b3a9448dded8b50
albas <albastru0227@gmail.com>
2026-05-27 10:23:06 +0900

    IGWの作成

--- network.tf
+++ network.tf
@@ -1,3 +1,4 @@
+# VPCの設定
 resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr
   enable_dns_support = true #DNS名前解決をできるようにする
@@ -7,6 +8,7 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
+# サブネットの設定
 resource "aws_subnet" "my_subnet_public_1a" {
   vpc_id = aws_vpc.my_vpc.id
   cidr_block = var.public_subnet_1a_cidr
@@ -40,5 +42,13 @@ resource "aws_subnet" "my_subnet_private_1c" {
   availability_zone = "ap-northeast-1c"
   tags = {
     Name = "my_subnet_private_1c"
+  }
+}
+
+# IGWの設定
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my_igw"
   }
 }
