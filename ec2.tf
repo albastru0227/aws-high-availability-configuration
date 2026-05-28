2a1503c8a25e1dadb0ea838fd8f089ed2bf347b1
albas <albastru0227@gmail.com>
2026-05-28 16:40:53 +0900

    踏み台サーバーに自動で秘密鍵ファイルを転送

--- ec2.tf
+++ ec2.tf
@@ -34,3 +34,32 @@ resource "aws_instance" "ec2_web" {
     Name = "web_1a"
   }
 }
+
+#踏み台サーバーへ自動で秘密鍵を転送する設定
+resource "null_resource" "transfer_keypair" {
+  #踏み台EC2が作成されることをトリガーとして実行されることを定義する
+  triggers = {
+    instance_id = aws_instance.ec2_bastion.id
+  }
+
+  #秘密鍵ファイルを踏み台サーバーの.sshディレクトリへ転送
+  provisioner "file" {
+    source = "C:/Users/albas/Downloads/my-keypair.pem"
+    destination = "/home/ec2-user/.ssh/my-keypair.pem"
+  }
+
+  #転送した秘密鍵ファイルの権限を600に変更
+  provisioner "remote-exec" {
+    inline = [ 
+      "chmod 600 /home/ec2-user/.ssh/my-keypair.pem"
+     ]
+  }
+
+  #踏み台サーバーへの接続設定
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    private_key = file("C:/Users/albas/Downloads/my-keypair.pem")
+    host = aws_instance.ec2_bastion.public_ip
+  }
+}
