b82fb54ecc8d59bea7e7ef1759675058238ded3d
albas <albastru0227@gmail.com>
2026-05-28 15:30:12 +0900

    outputs.tfにWebサーバーのプライベートIPを表示するように追加

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "alb_dns" {
   value = aws_alb.my_alb.dns_name
 }
 
-output "ec2_public_ip" {
+output "ec2_public_ip_bastion" {
   value = aws_instance.ec2_bastion.public_ip
+}
+
+output "ec2_private_ip_web" {
+  value = aws_instance.ec2_web.private_ip
 }
